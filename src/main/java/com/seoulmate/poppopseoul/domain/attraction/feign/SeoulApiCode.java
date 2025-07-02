a88d9c36f00c146372eeece2c905233a4a92c17e
ssshyn <ssshyn.dev@gmail.com>
2025-07-02 09:19:03 +0900

    주석 수정

--- src/main/java/com/seoulmate/poppopseoul/domain/attraction/feign/SeoulApiCode.java
+++ src/main/java/com/seoulmate/poppopseoul/domain/attraction/feign/SeoulApiCode.java
@@ -9,10 +9,11 @@ import lombok.Getter;
 public enum SeoulApiCode {
 //    TOUR_SPOT("TbVwAttractions", "관광명소 api", TourSpotFeignResponse.class),
 //    NIGHT_VIEW("viewNightSpot", "야경명소 api", NightViewFeignResponse.class),
-    MOUNTAIN_PARK("SebcParkTourKor", "산과공원 api", MountainParkResponse.class, 1, 1000);
 //    HANOK("ViewBcgImpFacil", "공공한옥 api", HanokFeignResponse .class),
 //    TOUR_ROAD("SebcTourStreetKor", "관광거리 api", TourRoadFeignResponse.class),
 //    CULTURAL("culturalEventInfo", "문화행사 api", CulturalFeignResponse.class);
+
+    MOUNTAIN_PARK("SebcParkTourKor", "산과공원 api", MountainParkResponse.class, 1, 1000);
 
     final String apiCode;
     final String description;
